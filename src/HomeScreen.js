5e2a9e43c9d4225ee6074b76b923fe7ebf2e6fa9
dawnbergdev <mmos7239@gmail.com>
2022-01-17 07:59:48 +0200

    banner design

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,13 +1,13 @@
 import React from 'react'
+import Banner from './Banner';
 import "./HomeScreen.css";
 import Nav from './Nav';
 
 function HomeScreen(){
     return  <div className='homeScreen'>
         <Nav />
-
-        {/* Banner */}
-
+  
+        <Banner />
         {/* Row */}
 
 
