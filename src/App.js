f9fa7b3361b236570b2de74b50cec39b0c64bf70
dawnbergdev <mmos7239@gmail.com>
2022-01-19 07:07:33 +0200

    created Home Screen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import './App.css';
-import HomeScreen from './HomeScreen';
+import HomeScreen from './screens/HomeScreen';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import LoginScreen from './screens/LoginScreen';
 
 function App() {
+  const user = null;
   return (
+    <BrowserRouter>
     <div className="app">
-      <HomeScreen />
+      {!user ? (<LoginScreen/>) : (
+        <Routes>
+        <Route exact path="/"
+        element={<HomeScreen/>}>
+        </Route>
+      </Routes>
+      )}
+      
     </div>
+    </BrowserRouter>
+
   );
 }
 
